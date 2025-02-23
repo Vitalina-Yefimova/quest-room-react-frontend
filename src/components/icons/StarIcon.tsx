30d80a143c12cd8b215747bb308210b3a439d65f
Vitalina Yefimova <vitalina.yefimova@icloud.com>
2025-02-23 01:51:43 -0700

    added images+icons, made SVGs separate components

---
+++ src/components/icons/StarIcon.tsx
@@ -0,0 +1,21 @@
+const StarIcon = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g id="icon / Star">
+      <path
+        id="Star"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7 1L5.33333 5.8H1L4.66667 8.88571L3.33333 13L7 10.2567L10.6667 13L9.33333 8.88571L13 5.8H8.66667L7 1Z"
+        fill="white"
+      />
+    </g>
+  </svg>
+);
+export default StarIcon;
---
+++ src/components/icons/VerticalDividerIcon.tsx
@@ -0,0 +1,18 @@
+const VerticalDividerIcon = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 2 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      id="&#209;&#128;&#208;&#176;&#208;&#183;&#208;&#180;&#208;&#181;&#208;&#187;&#208;&#184;&#209;&#130;&#208;&#181;&#208;&#187;&#209;&#140;"
+      d="M1 0V20"
+      stroke="white"
+      strokeOpacity={0.5}
+    />
+  </svg>
+);
+export default VerticalDividerIcon;
